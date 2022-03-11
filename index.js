d448803d518d14e497babbca783bbb147d682ec1
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2022-03-12 00:45:38 +0900

    UPDATE: module -> cjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import { firebase } from '@firebase/app';
-import '@firebase/firestore';
+const express = require('express');
+const cors = require('cors');
+const {firebase} = require('@firebase/app');
+require('@firebase/firestore');
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
